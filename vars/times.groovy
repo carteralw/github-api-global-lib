6b0ebf95650df7d6e496f292f753f53ef24635b4
Al Carter <carteralw@gmail.com>
2024-09-11 14:13:35 -0400

    adding new time task

---
+++ vars/times.groovy
@@ -0,0 +1,8 @@
+def timed(String stepName, Closure body) {
+    return {
+        def start = System.currentTimeMillis()
+        body()
+        def duration = System.currentTimeMillis() - start
+        echo "${stepName} took ${duration}ms"
+    }
+}
